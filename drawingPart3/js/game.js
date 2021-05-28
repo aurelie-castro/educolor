127a4e1d13d1be1847ceb55a37c565e7e61eb3fc
aurelie-castro <acdemarly@gmail.com>
2021-05-28 10:58:53 +0200

    added eraser for light blue brush

--- drawingPart3/js/game.js
+++ drawingPart3/js/game.js
@@ -285,7 +285,17 @@ function create ()
             }
 
             if(pointer.isDown && blueLightColor === true){
-                this.add.image(pointer.x, pointer.y, 'blueLight', Phaser.Math.Between(0, 10.3));
+//                this.add.image(pointer.x, pointer.y, 'blueLight', Phaser.Math.Between(0, 10.3));
+                if(stopColoring === false){
+                    testIDK = this.add.image(pointer.x, pointer.y, 'blueLight').setInteractive();
+                }
+                testIDK.on('pointermove', function(pointer){
+                     if(erasingColors === true){
+                        this.destroy();
+                         console.log(pointer.x);
+//                         blackColor = false;
+                         stopColoring = true;
+                     }});
             }
 
             if(pointer.isDown && orangeColor === true){
@@ -419,6 +429,9 @@ function create ()
             greyColor = false; 
             blueLightColor = true;
             pinkColor = false;
+            
+            stopColoring = false;
+            erasingColors = false;
         }
         
         if(pointer.x >= 15 && pointer.x <= 45  && pointer.y >= 20 && pointer.y <=47){
